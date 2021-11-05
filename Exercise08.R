6ba665a6e1718755d253f6dfc06fc9869803eb3f
ashtonbieri <abieri@nd.edu>
2021-11-05 11:02:10 -0400

    Initial comments and begin problem 1

---
+++ Exercise08.R
@@ -0,0 +1,14 @@
+### Biocomputing -- Exercise 08
+### Ashton Bieri | github @ashtonbieri
+
+### Problem 1: Generate summary plot for UW v MSU sample data
+import(ggplot2)
+UWvMSU<-read.table("UWvMSU_1-22-13.txt",sep="")
+
+### Problem 2: "Guess my number" game
+
+# create vector of numbers 1-100
+
+# pick random number from list
+
+## loop: prompt for guess, respond lower, higher, or correct up to 10 guesses
